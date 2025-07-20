34d3956e59f54e8e26318cd612a10fb62de671dc
sonujain299 <sonujain.299@gmail.com>
2025-07-20 10:50:17 +0000

    added controller action for calculating sum

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,4 +1,14 @@
 class CalculatorController < ApplicationController
   def index
   end
+
+  def calculate
+    input = params[:input] || ""
+    begin
+      @result = StringCalculator.add(input)
+    rescue => e
+      @error = e.message
+    end
+    render :index
+  end
 end
