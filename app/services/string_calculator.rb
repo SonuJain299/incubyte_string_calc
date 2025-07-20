c28ad85c2a5a33b0735a0572a0c9972795b3d566
sonujain299 <sonujain.299@gmail.com>
2025-07-20 14:09:48 +0000

    added logic for negative numbers

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,8 +1,10 @@
 class StringCalculator
   def self.add(input)
     return 0 if input.empty?
-    delimiter = ','
+    delimiter = /,|\n/
     numbers = input.split(delimiter).map(&:to_i)
-    return numbers.sum
+    negatives = numbers.select { |n| n < 0 }
+    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
+    numbers.sum
   end
 end
