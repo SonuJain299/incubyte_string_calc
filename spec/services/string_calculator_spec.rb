9aa26f76876b5774414ca5016afc279a2bdaaa16
sonujain299 <sonujain.299@gmail.com>
2025-07-20 12:56:19 +0000

    added some specs

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -18,5 +18,9 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("1,2,3")).to eq(6)
     end
 
+    it 'handles newlines as delimiter' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
     
 end
