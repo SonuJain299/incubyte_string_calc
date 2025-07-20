8187b9cc98c8d4b25a1e224b228c600079d444a9
sonujain299 <sonujain.299@gmail.com>
2025-07-20 15:19:28 +0000

    finalising code

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -2,6 +2,11 @@ class StringCalculator
   def self.add(input)
     return 0 if input.empty?
     delimiter = /,|\n/
+    if input.start_with?("//")
+      parts = input.split("\n", 2)
+      delimiter = Regexp.new(Regexp.escape(parts[0][2..]))
+      input = parts[1]
+    end
     numbers = input.split(delimiter).map(&:to_i)
     negatives = numbers.select { |n| n < 0 }
     raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
