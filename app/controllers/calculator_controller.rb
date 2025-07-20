3805afc4c564f9f910bfb6a22e8e7f141ef89f52
sonujain299 <sonujain.299@gmail.com>
2025-07-20 12:50:43 +0000

    added spec for multiple numbers

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,11 +1,15 @@
 class CalculatorController < ApplicationController
   def index
+    @input = ""
+    puts @result
+    @error = nil
   end
 
   def calculate
     input = params[:input] || ""
     begin
       @result = StringCalculator.add(input)
+      puts "Calulation result: #{@result}" # Debugging output
     rescue => e
       @error = e.message
     end
--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,5 +1,8 @@
 class StringCalculator
   def self.add(input)
     return 0 if input.empty?
+    delimiter = ','
+    numbers = input.split(delimiter).map(&:to_i)
+    return numbers.sum
   end
 end
